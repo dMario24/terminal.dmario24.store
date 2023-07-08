d1c191cd3710a155ac4ea171f6d4a1848d4534ee
dMario24 <data.mario24@gmail.com>
2023-07-08 15:46:04 +0900

    draft - add command ls

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -41,6 +41,7 @@ export const commands: Command = [
   { cmd: "themes", desc: "check available themes", tab: 7 },
   { cmd: "welcome", desc: "display hero section", tab: 6 },
   { cmd: "whoami", desc: "about current user", tab: 7 },
+  { cmd: "ls", desc: "about current user", tab: 11 },
 ];
 
 type Term = {
